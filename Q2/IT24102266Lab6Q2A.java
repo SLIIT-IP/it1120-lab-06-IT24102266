78de8b8ffc75661809eca01cee1dd16071282ee7
IT24102266 <it24102266@my.sliit.lk>
2024-08-28 11:50:52 +0530

    IT24102266Lab6Q2A

---
+++ Q2/IT24102266Lab6Q2A.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class IT24102266Lab6Q2A
+{
+ public static void main(String args[])
+ { 
+   int num= 1;
+   
+    
+   while (num<=10)
+   {
+     System.out.print(num +" "  );
+     num++;
+   }
+  }
+}
